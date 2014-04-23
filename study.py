8e7369e133abb792ece425b5f3a19f52d0341496
Rajkumar R <rraj.be@gmail.com>
2014-04-23 06:27:05 +0530

    Adding statistics printing

--- study.py
+++ study.py
@@ -17,15 +17,32 @@ if __name__ == "__main__":
 		print_summary()
 		sys.exit()
 
+	known = 0
+	studied = 0
+	words = 0
+
 	fp = open(sys.argv[1],'r')
 	l = WordNetLemmatizer()
 
 	barrons = fp.read()
+	for word in barrons.split():
+		words = words + 1
+	print "Total words : %d" %(words)
 
 	for word in barrons.split():
 		if len(wn.synsets(word)) is not 0:
 			rlemma = l.lemmatize(word)
 			opt = raw_input( "Display %s : %s?  :   " % (word,l.lemmatize(word)))
 			if opt == 'y':
+				studied = studied+1
 				for ss in wn.synsets(word):
 					print "%20s : %s\n" % (word,ss.definition)
+			if opt == 'e':
+			  	print "Current streak : %d %d" % (studied,known)
+				sys.exit()
+			else:
+				known = known+1
+
+	
+	print "Current streak : %d %d" % (studied,known)
+	sys.exit()
