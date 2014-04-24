53b984ac7b93dd747fbb59603f90f88bc0090dbe
Rajkumar R <rraj.be@gmail.com>
2014-04-24 05:41:39 +0530

    Adding feature to obtain description from vocabulary.com

--- study.py
+++ study.py
@@ -1,6 +1,9 @@
-import sys
-from nltk.corpus import wordnet as wn
-from nltk.stem.wordnet import WordNetLemmatizer
+import sys					# for system io
+import urllib2					# for url parssing et al
+import textwrap					# To limit o/p characters per line
+from nltk.corpus import wordnet as wn		# Wordnet DB
+from nltk.stem.wordnet import WordNetLemmatizer	# To Obtain Lemma
+from BeautifulSoup import BeautifulSoup		
 
 def print_summary():
 	print "\n \
@@ -33,13 +36,30 @@ if __name__ == "__main__":
 		if len(wn.synsets(word)) is not 0:
 			rlemma = l.lemmatize(word)
 			opt = raw_input( "Display %s : %s?  :   " % (word,l.lemmatize(word)))
-			if opt == 'y':
+			if opt == 'p':
 				studied = studied+1
 				for ss in wn.synsets(word):
 					print "%20s : %s\n" % (word,ss.definition)
 			if opt == 'e':
 			  	print "Current streak : %d %d" % (studied,known)
 				sys.exit()
+			if opt == 'm':
+				studied = studied+1
+				for ss in wn.synsets(word):
+					print "%20s : %s\n" % (word,ss.definition)
+			  	url="http://www.vocabulary.com/dictionary/"+word
+				response = urllib2.urlopen(url)
+				replace = ["\"","<i>","</i>","<p class=long>","<p class=short>","</p>"]
+				html = response.read()
+				soup = BeautifulSoup(html)
+				rshort = soup.findAll(attrs={"class" : "short"})
+				rlong = soup.findAll(attrs={"class" : "long"})
+				rlong = str(rlong[0])
+				rshort = str(rshort[0])
+				for rep in replace:
+					rlong=rlong.replace(rep,"")
+					rshort = rshort.replace(rep,"")
+				print "%s\n\n%s\n\n" % (textwrap.fill(rshort, width=100),textwrap.fill(rlong, width=100))
 			else:
 				known = known+1
 
